8bb2f5f21275e04c43593695abde28029124440c
velayosjoel <36230548+velayosjoel@users.noreply.github.com>
2018-02-14 12:29:45 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,33 @@
+function Mostrar()
+{
+//tomo el mes
+var mesDelAño = document.getElementById('mes').value;
+
+switch (mesDelAño )
+
+{
+
+    case "Enero": alert ("que comiences bien el año") 
+
+      break ;
+
+        case "Marzo": alert(" a clases!! " )
+
+         break ;
+
+            case "Julio" : alert (" se vienen las vacaciones")
+
+             break ;
+
+                case "Diciembre" : alert ("felices fiestas ")
+
+                  break ;
+                    
+   
+
+}
+
+
+
+}
+//FIN DE LA FUNCIÓN
---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,48 @@
+function Mostrar()
+{
+//tomo el mes 
+
+
+var mesDelAño = document.getElementById('mes').value;
+
+switch (mesDelAño )
+
+{
+
+    case "Enero": alert ("Ya pasamos el frio, ahora calor!!") 
+
+      break ;
+
+        case "Febrero": alert("Ya pasamos el frio, ahora calor!! " )
+
+         break ;
+
+            case "Marzo" : alert ("Ya pasamos el frio, ahora calor!!s")
+
+             break ;
+
+                case "Abril" : alert ("Falta para el invierno. ")
+
+                  break ;
+                    
+                    case "Mayo" : alert ("Falta para el invierno. ")
+
+                        break ;
+
+                             case "Junio" : alert ( " Abrigate que hace frio. ")
+
+                             break ;
+
+                             case "Julio" : alert ("Abrigate que hace frio. ")
+
+                            break ;
+
+                            case "Agosto" : alert (" Abrigate que hace frio.")
+
+                            break ;
+
+                            case " Septiembre " : alert  (" ")
+   
+
+
+}//FIN DE LA FUNCIÓN
